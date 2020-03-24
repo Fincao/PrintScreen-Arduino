f9f7f66b4bd8c0f72deff23f57e60fc28a67f06c
William Pedro de Oliveira <62478186+Fincao@users.noreply.github.com>
2020-03-24 18:41:04 -0300

    Sketch PrintScreen Win10

---
+++ print.ino
@@ -0,0 +1,54 @@
+#include "Keyboard.h"
+#include "HIDKeyboard.h"
+
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+/* Inicia o script */
+void setup()
+{
+  Keyboard.begin();
+
+  //FAZ O PRINTSCREEN NO WINDOWS 10.
+  delay(2000);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press('s');
+  Keyboard.releaseAll(); 
+  delay(1000);
+   typeKey(KEY_TAB);
+   delay(200);
+   typeKey(KEY_TAB);
+   delay(200);
+   typeKey(KEY_TAB);
+   delay(200);
+   typeKey(KEY_TAB);
+   delay(200); 
+   typeKey(KEY_RETURN); 
+  //FIM DO PRINTSCREEN, O PRINT AGORA ESTÁ NA AREA DE TRANFERENCIA DO WINDOWS.
+
+  // O RESTO DO CÓDIGO É SÓ UMA PROVA DE QUE FUNCIONA...        
+    
+  // ABRE O PAINT
+  delay(200);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(200);
+  Keyboard.print("mspaint");
+  typeKey(KEY_RETURN);
+
+  // COLA O PRINTSCREEN
+  delay(3200);
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press('v');
+  delay(100);
+  Keyboard.releaseAll();
+
+  Keyboard.end();
+}
+void loop() {}
